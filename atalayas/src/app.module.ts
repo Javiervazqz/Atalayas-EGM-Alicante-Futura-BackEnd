f611b1236eb5bcdb2bda390af9dcd6ac035dc245
Óscar Sala <153222887+OscarSala96@users.noreply.github.com>
2026-03-05 13:31:17 +0100

    schema conectado con db

--- atalayas/src/app.module.ts
+++ atalayas/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupabaseModule } from './supabase/supabase.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { SupabaseService } from './supabase/supabase.service';
 
 @Module({
-  imports: [],
+  imports: [SupabaseModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SupabaseService],
 })
 export class AppModule {}
