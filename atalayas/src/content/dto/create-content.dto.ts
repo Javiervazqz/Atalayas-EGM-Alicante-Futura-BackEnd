bedd5e145caf39d8f582a1c5e7cc39f3149e22f6
Óscar Sala <153222887+OscarSala96@users.noreply.github.com>
2026-03-12 13:26:16 +0100

    update db

--- atalayas/src/content/dto/create-content.dto.ts
+++ atalayas/src/content/dto/create-content.dto.ts
@@ -11,19 +11,4 @@ export class CreateContentDto {
   @IsNotEmpty()
   title: string;
 
-  @ApiPropertyOptional({
-    example: 'https://midominio.com/tema1.pdf',
-    description: 'URL del PDF',
-  })
-  @IsUrl()
-  @IsOptional()
-  pdfUrl?: string;
-
-  @ApiPropertyOptional({
-    example: 'https://youtube.com/video',
-    description: 'URL del vídeo',
-  })
-  @IsUrl()
-  @IsOptional()
-  videoUrl?: string;
 }
--- atalayas/src/content/dto/update-content.dto.ts
+++ atalayas/src/content/dto/update-content.dto.ts
@@ -12,22 +12,6 @@ export class UpdateContentDto extends PartialType(CreateContentDto) {
   @IsOptional()
   title?: string;
 
-  @ApiPropertyOptional({
-    example: 'https://midominio.com/tema1.pdf',
-    description: 'URL del PDF',
-  })
-  @IsUrl()
-  @IsOptional()
-  pdfUrl?: string;
-
-  @ApiPropertyOptional({
-    example: 'https://youtube.com/video',
-    description: 'URL del vídeo',
-  })
-  @IsUrl()
-  @IsOptional()
-  videoUrl?: string;
-
   @ApiProperty({ example: 1, description: 'Orden de aparición en el curso' })
   @IsInt()
   @IsOptional()
